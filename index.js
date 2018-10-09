287d62068fb8b96eabf942c7cdbd97ff423f5078
BraulioSanchez <braulio.sanchez@usp.br>
2018-10-08 21:41:45 -0500

    editing

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hi I\'m a chatbot')
 })
 
 let token = ''
 
-app.get('/webhook/', function(req, res) {
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'ch4tb0t') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Wrong token')
 })
 
-/*app.post('/webhook/', function(req, res) {
+app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
-    for(let i = 0; i<messaging_events.length; i++) {
+    for (let i = 0; i < messaging_events.length; i++) {
         let event = messaging_events[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
@@ -38,11 +38,24 @@ app.get('/webhook/', function(req, res) {
 })
 
 function sendText(sender, text) {
-    let messageData = {text: text}
+    let messageData = { text: text }
     request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token, token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('sending error')
+        } else if (response.body.error){
+            console.log('reponse body error')
+        }
     })
-}*/
+}
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running: port')
 })
